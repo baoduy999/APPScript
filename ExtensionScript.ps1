a8e6b3a76376f5c120fb6fdcec30b01c0e8b1851
baoduy999 <75001116+baoduy999@users.noreply.github.com>
2020-11-25 10:15:22 +0700

    Add files via upload

---
+++ ExtensionScript.ps1
@@ -0,0 +1,6 @@
+﻿New-Item -Path "c:\" -Name "MediaUnzipped" -ItemType "directory"
+New-Item -Path "c:\" -Name "GeneratedMedia" -ItemType "directory"
+New-Item -Path "c:\" -Name "MediaUnzipped" -ItemType "directory"
+New-Item -Path "c:\" -Name "temp" -ItemType "directory"
+Invoke-WebRequest https://github.com/Azure/azure-powershell/releases/download/v5.1.0-November2020/Az-Cmdlets-5.1.0.33698-x64.msi -OutFile C:\temp\Az-Cmdlets-5.1.0.33698-x64.msi
+Start-Process C:\temp\Az-Cmdlets-5.1.0.33698-x64.msi -ArgumentList '/quiet'
